eba85db52df778a07d6c118220d77ab0e38df21e
Nikita Dragunov <11559709+Niatik@users.noreply.github.com>
2019-02-27 16:49:17 +0600

    Fix 'Context name' in test connecting to access database.

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -27,30 +27,6 @@ var _ = Describe("Report Utils", func() {
 		})
 	})
 
-	//Context("Send Vzljot 031", func() {
-	//	excelFileName := "Data1901.xlsx"
-	//	xlFile, _ := xlsx.OpenFile(excelFileName)
-	//
-	//	sheet := xlFile.Sheet["719138"]
-	//
-	//	model, _ := sheet.Cell(0, 0).String()
-	//
-	//	application, _ := sheet.Cell(0, 1).String()
-	//
-	//	serial, _ := sheet.Cell(0, 2).String()
-	//
-	//	var c ConcreteCreator
-	//	device := c.CreateDevice(model, application)
-	//	c.RegisterDevice(device)
-	//
-	//	device.SetSerial(serial)
-	//	device.SetSheet(*sheet)
-	//	err:=device.Send()
-	//	It("has the table in database", func() {
-	//		Expect(err).Should(BeNil())
-	//	})	
-	//})
-
 	Context("Exist Table", func() {
 		_, dbServer, dbUser, dbPassword, dbName, _ := LoadConfig()
 		db := ConnectMSSQL(dbServer, dbUser, dbPassword)
@@ -68,7 +44,7 @@ var _ = Describe("Report Utils", func() {
 		})
 	})
 
-	Context("Connect MS SQL Database", func() {
+	Context("Connect MS Access Database", func() {
 		pathAccess, _, _, _, _, _ := LoadConfig()
 		db := ConnectMSACCESS(pathAccess)
 		It("can connect to database", func() {
